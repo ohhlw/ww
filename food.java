bed05923b3a26bbf5514ba4ce5061a7900c34334
ohhlw <1750193308@qq.com>
2021-01-11 19:05:32 +0800

    commit 2

--- food.java
+++ food.java
@@ -2,4 +2,5 @@ import java.util.*
 class food{ 
     private string foodsort;
     pirvate string foodname;
+    private string calorie //食物卡路里
 }
---
+++ meat.java
@@ -0,0 +1,5 @@
+import java.util.*;
+class meat{
+  String meatsort; //肉类
+  String mname;   //肉名称
+}
